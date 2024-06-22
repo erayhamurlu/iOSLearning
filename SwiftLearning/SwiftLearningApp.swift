cddc596a42f6b166cbad971a2f067e372edf79fa
Eray Hamurlu <hamurlueray@gmail.com>
2024-06-22 19:46:04 +0300

    Initial Commit

---
+++ SwiftLearning/SwiftLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftLearningApp.swift
+//  SwiftLearning
+//
+//  Created by Eray hamurlu on 22.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
