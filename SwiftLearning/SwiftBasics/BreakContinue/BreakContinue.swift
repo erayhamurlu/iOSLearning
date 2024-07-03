aefc07fd6bbc5140e1d101461d3bc4ce64468bb3
Eray Hamurlu <hamurlueray@gmail.com>
2024-07-03 20:36:38 +0300

    Update Break&Continue

--- SwiftLearning/SwiftBasics/BreakContinue/BreakContinue.swift
+++ SwiftLearning/SwiftBasics/BreakContinue/BreakContinue.swift
@@ -5,7 +5,6 @@
 //  Created by Eray hamurlu on 3.07.2024.
 //
 
-import Foundation
 import SwiftUI
 
 struct BreakContinue: View {
@@ -14,30 +13,35 @@ struct BreakContinue: View {
     
     var body: some View {
         VStack {
+            // Buton oluşturuluyor. Butona basıldığında detectNumber fonksiyonu çağrılacak.
             Button("Detect Numbers") {
                 detectNumber()
             }
         }
     }
     
-    
+    // Dizideki sayıları kontrol eden fonksiyon
     func detectNumber()  {
         for number in numbers {
+            // Eğer sayı 20'den büyükse, mesaj yazdırılır ve döngü sonlandırılır (break).
             if number > 20 {
                 print("20 sayısı bulundu")
                 break
             }
             
+            // Eğer sayı 10'dan küçükse, mesaj yazdırılır ve döngünün geri kalan kısmı atlanarak devam edilir (continue).
             if number < 10 {
                 print("10'dan küçük sayılar: \(number)")
                 continue
             }
             
+            // Yukarıdaki koşullara uymayan sayılar için mesaj yazdırılır.
             print("Şu anki sayı: \(number)")
         }
     }
 }
 
+// Preview yapılandırması, SwiftUI önizlemesi için kullanılır.
 #Preview {
     BreakContinue()
 }
