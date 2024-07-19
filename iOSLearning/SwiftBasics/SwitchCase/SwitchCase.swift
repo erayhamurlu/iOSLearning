da8bd189ee88d854234a23fb6f68c29280767c1d
Eray Hamurlu <hamurlueray@gmail.com>
2024-07-19 10:21:46 +0300

    SwitchCase update

--- iOSLearning/SwiftBasics/SwitchCase/SwitchCase.swift
+++ iOSLearning/SwiftBasics/SwitchCase/SwitchCase.swift
@@ -8,35 +8,34 @@
 import SwiftUI
 
 struct SwitchCase: View {
-    var dayOfWeek: String = "Pazartesi" // Haftanın günlerinden biri
-
+    @State var dayOfWeek: String = "Salı" // Haftanın günlerinden biri
+    
     var body: some View {
         VStack {
             // MARK: Gün değerine göre mesaj belirleme
-            Text(messageForDay(dayOfWeek: dayOfWeek))
+            Text(dayOfWeek)
                 .padding()
-        }
-    }
-
-    // Haftanın gününe göre mesaj döndüren fonksiyon
-    func messageForDay(dayOfWeek: String) -> String {
-        switch dayOfWeek {
-        case "Pazartesi":
-            return "Haftanın başlangıcı, enerjik olun!"
-        case "Salı":
-            return "İkinci gün, hız kesmeden devam!"
-        case "Çarşamba":
-            return "Ortaya geldik, azimle çalış!"
-        case "Perşembe":
-            return "Perşembe, hafta sonuna az kaldı!"
-        case "Cuma":
-            return "Son iş günü, hafta sonu yaklaşıyor!"
-        case "Cumartesi":
-            return "Hafta sonu keyfi, iyi dinlen!"
-        case "Pazar":
-            return "Pazar, yeni haftaya hazırlık!"
-        default:
-            return "Geçersiz gün!"
+            
+            Button("Günü Güncelle") {
+                switch dayOfWeek {
+                    case "Pazartesi":
+                        dayOfWeek = "Haftanın başlangıcı, enerjik olun!"
+                    case "Salı":
+                        dayOfWeek = "İkinci gün, hız kesmeden devam!"
+                    case "Çarşamba":
+                        dayOfWeek = "Ortaya geldik, azimle çalış!"
+                    case "Perşembe":
+                        dayOfWeek = "Perşembe, hafta sonuna az kaldı!"
+                    case "Cuma":
+                        dayOfWeek = "Son iş günü, hafta sonu yaklaşıyor!"
+                    case "Cumartesi":
+                        dayOfWeek = "Hafta sonu keyfi, iyi dinlen!"
+                    case "Pazar":
+                        dayOfWeek = "Pazar, yeni haftaya hazırlık!"
+                    default:
+                        dayOfWeek = "Geçersiz gün!"
+                }
+            }
         }
     }
 }
